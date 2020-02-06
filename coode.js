41054b01ebfb4fcadec726b88ae281dd53e899da
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-06 22:24:40 +0100

    ciekawe co z tego wyjdzie

--- coode.js
+++ coode.js
@@ -1,1 +1,23 @@
-function maskify(cc)
+function maskify(cc){
+    if (cc.length>4){
+// var leng=cc.length
+// var last=cc.slice(leng-4);
+// var a=cc.slice(0, leng-4);
+
+var tabCC =[...cc];
+
+var a=cc.length - 4;
+
+
+var tab2 =[];
+tab2.fill("#", 0, a+4);
+}
+
+
+return join(slice.tabCC(a), tab2);
+        
+    
+        }
+        else {return cc;}
+}
+document.write(maskify("maciek"));
