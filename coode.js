f7342499b47aee06d1e190c9d26f2cdcc42dfa7b
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-06 22:27:29 +0100

    z komentarzami

--- coode.js
+++ coode.js
@@ -4,18 +4,15 @@ function maskify(cc){
 // var last=cc.slice(leng-4);
 // var a=cc.slice(0, leng-4);
 
-var tabCC =[...cc];
+var tabCC =[...cc]; //tablica z hasla
 
-var a=cc.length - 4;
+var a=cc.length - 4; //ilosc #
 
-
-var tab2 =[];
-tab2.fill("#", 0, a+4);
-}
-
+var tab2 =[]; //pusta tablica do wypelnienia #
+tab2.fill("#", 0, a+3);
 
 return join(slice.tabCC(a), tab2);
-        
+    }        
     
         }
         else {return cc;}
