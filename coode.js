553df21ca3b63cc81115033c8a4452685454208a
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-07 22:25:35 +0100

    wreszcie!

--- coode.js
+++ coode.js
@@ -1,20 +1,25 @@
 function maskify(cc) {
     if (cc.length > 4) {
+        
+        var a = cc.length - 4; //ilosc # do wyświetlenia
 
+        var tab2 =new Array(a); //pusta tablica do wypelnienia #
+        tab2.fill("#"); //tablica z #
 
-        var tabCC = [cc]; //tablica z hasla
+        var tab2Str=tab2.join(""); //string z #
 
-        var a = cc.length - 4; //ilosc #
+        var hashTab= cc.split(""); //tablica z hasła
+        hashTab.reverse();         //odwrócenie tablicy z hasła
+        
+        hashTab=hashTab.slice(0, 4); //ostatnie 4 litery hasła
+        hashTab.reverse();           //pierwotna kolejność
 
-        var tab2 = []; //pusta tablica do wypelnienia #
-        tab2.fill("#", 0, a + 3);
+        var hashStr=hashTab.join("",4); //string z 4 ostatnich
 
-        var hashTab= tabCC.slice(a);
-
-        console.log( toString(hashTab) + toString(tab2) );
+        console.log(tab2Str+hashStr);  //string z całości
     }
 
     else { console.log(cc); }
 
 }
-maskify("Maciek");
+maskify("a0123456789");
