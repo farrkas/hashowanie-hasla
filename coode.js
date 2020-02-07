e6ce4747c413c15e7bc011b1e179e3bfc85e6422
Maciek Lorencowicz <m.lorencowicz@gmail.com>
2020-02-07 21:19:32 +0100

    poprawa?

--- coode.js
+++ coode.js
@@ -1,18 +1,20 @@
-function maskify(cc){
-    if (cc.length>4){
+function maskify(cc) {
+    if (cc.length > 4) {
 
 
-var tabCC =[...cc]; //tablica z hasla
+        var tabCC = [cc]; //tablica z hasla
 
-var a=cc.length - 4; //ilosc #
+        var a = cc.length - 4; //ilosc #
 
-var tab2 =[]; //pusta tablica do wypelnienia #
-tab2.fill("#", 0, a+3);
+        var tab2 = []; //pusta tablica do wypelnienia #
+        tab2.fill("#", 0, a + 3);
 
-console.log(slice.tabCC(a) + toString(tab2);   
-            }
+        var hashTab= tabCC.slice(a);
 
-        else {console.log(cc);}
+        console.log( toString(hashTab) + toString(tab2) );
+    }
+
+    else { console.log(cc); }
 
 }
-console.log(maskify("maciek"));
+maskify("Maciek");
